000eedc057e305377aea3190935343119a87e1b5
Ruedi Steinmann <ruedi.steinmann@ecamos.ch>
2017-10-05 08:40:04 +0200

    add isScopeActive

--- core/src/main/java/com/github/ruediste/salta/standard/util/SimpleScopeManagerBase.java
+++ core/src/main/java/com/github/ruediste/salta/standard/util/SimpleScopeManagerBase.java
@@ -9,70 +9,74 @@ import com.google.common.collect.Maps;
 
 public abstract class SimpleScopeManagerBase implements ScopeHandler {
 
-    protected final ThreadLocal<ScopeState> currentState = new ThreadLocal<>();
-    protected final String scopeName;
+	protected final ThreadLocal<ScopeState> currentState = new ThreadLocal<>();
+	protected final String scopeName;
 
-    public SimpleScopeManagerBase(String scopeName) {
-        this.scopeName = scopeName;
-    }
+	public SimpleScopeManagerBase(String scopeName) {
+		this.scopeName = scopeName;
+	}
 
-    public static class ScopeState {
-        Map<Binding, Object> data = Maps.newHashMap();
+	public static class ScopeState {
+		Map<Binding, Object> data = Maps.newHashMap();
 
-        ScopeState() {
-        }
-    }
+		ScopeState() {
+		}
+	}
 
-    public void inScopeDo(ScopeState state, Runnable runnable) {
-        ScopeState old = setState(state);
-        try {
-            runnable.run();
-        } finally {
-            setState(old);
-        }
-    }
+	public void inScopeDo(ScopeState state, Runnable runnable) {
+		ScopeState old = setState(state);
+		try {
+			runnable.run();
+		} finally {
+			setState(old);
+		}
+	}
 
-    public Optional<Map<Binding, Object>> tryGetValueMap() {
-        return Optional.ofNullable(currentState.get()).map(d -> d.data);
-    }
+	public Optional<Map<Binding, Object>> tryGetValueMap() {
+		return Optional.ofNullable(currentState.get()).map(d -> d.data);
+	}
 
-    /**
-     * Return the current value map.
-     * 
-     * @throws RuntimeException
-     *             if no scope is active
-     */
-    public Map<Binding, Object> getValueMap() {
-        return tryGetValueMap()
-                .orElseThrow(() -> new RuntimeException("Cannot access value map outside of scope " + scopeName));
-    }
+	/**
+	 * Return the current value map.
+	 * 
+	 * @throws RuntimeException
+	 *             if no scope is active
+	 */
+	public Map<Binding, Object> getValueMap() {
+		return tryGetValueMap()
+				.orElseThrow(() -> new RuntimeException("Cannot access value map outside of scope " + scopeName));
+	}
 
-    /**
-     * Set a fresh scope state.
-     * 
-     * @return the old scope state
-     */
-    public ScopeState setFreshState() {
-        return setState(createFreshState());
-    }
+	/**
+	 * Set a fresh scope state.
+	 * 
+	 * @return the old scope state
+	 */
+	public ScopeState setFreshState() {
+		return setState(createFreshState());
+	}
 
-    public ScopeState createFreshState() {
-        return new ScopeState();
-    }
+	public ScopeState createFreshState() {
+		return new ScopeState();
+	}
 
-    /**
-     * Set a given scope state
-     * 
-     * @param state
-     *            state to set, or null to set to an empty state
-     * @return the old scope state
-     */
-    public ScopeState setState(ScopeState state) {
-        ScopeState old = currentState.get();
-        if (state == null)
-            currentState.remove();
-        else
-            currentState.set(state);
-        return old;
-    }
+	public boolean isScopeActive() {
+		return currentState.get() != null;
+	}
+
+	/**
+	 * Set a given scope state
+	 * 
+	 * @param state
+	 *            state to set, or null to set to an empty state
+	 * @return the old scope state
+	 */
+	public ScopeState setState(ScopeState state) {
+		ScopeState old = currentState.get();
+		if (state == null)
+			currentState.remove();
+		else
+			currentState.set(state);
+		return old;
+	}
 }
